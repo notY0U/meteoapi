8961f037034d22f463b78a7898e293a688102f9b
Dovydas Gražys <57098864+notY0U@users.noreply.github.com>
2020-07-17 16:45:23 +0300

    create DB schema's #5 2/4 checkbox

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        $faker = Faker\Factory::create('lt_LT');
+        // seed of values for weather conditions
+        $tagers=['clear', 'isolated-clouds', 'scattered-clouds', 'overcast', 
+        'light-rain', 'moderate-rain', 'heavy-rain', 'sleet', 'light-snow',
+        'moderate-snow', 'heavy-snow', 'fog', 'na'];
+        foreach($tagers as $val) {
+            DB::table('tags')->insert([
+                'title' =>  $val
+               
+            ]);
+        }
+        // seed for products
+        // DB::table('products')->insert([
+        //     'sku'->$faker(),
+        //     'name'
+        //     'price'
+        // ]);
+        
     }
 }
+
