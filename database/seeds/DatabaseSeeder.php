c7bfe31aaec9a107bf99f39607adefb7480595ab
Dovydas Gražys <57098864+notY0U@users.noreply.github.com>
2020-07-18 13:05:47 +0300

    $faker seed for products and tags tables #9

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,24 +11,40 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $faker = Faker\Factory::create('lt_LT');
+        $faker = Faker\Factory::create('en_EN');
+        $faker->addProvider(new \RauweBieten\PhpFakerClothing\Clothing($faker));
+        $faker->addProvider(new Liior\Faker\Prices($faker));
+
         // seed of values for weather conditions
-        $tagers=['clear', 'isolated-clouds', 'scattered-clouds', 'overcast', 
-        'light-rain', 'moderate-rain', 'heavy-rain', 'sleet', 'light-snow',
-        'moderate-snow', 'heavy-snow', 'fog', 'na'];
-        foreach($tagers as $val) {
+        $tagers = ['clear', 'isolated-clouds', 'scattered-clouds', 'overcast',
+            'light-rain', 'moderate-rain', 'heavy-rain', 'sleet', 'light-snow',
+            'moderate-snow', 'heavy-snow', 'fog', 'na'];
+        foreach ($tagers as $val) {
             DB::table('tags')->insert([
-                'title' =>  $val
-               
+                'title' => $val,
+
             ]);
         }
+        //function to generate SKU for product
+        function sku($name)
+        {
+            $word = str_word_count($name,1);
+            $count = count($word);
+            $name=$word[$count-1];
+           
+                return substr($name, 0, 4) . '-' . rand(1, 100);
+
+        }
         // seed for products
-        // DB::table('products')->insert([
-        //     'sku'->$faker(),
-        //     'name'
-        //     'price'
-        // ]);
-        
+        foreach (range(1, 30) as $val) {
+            $x = $faker->clothing();
+            DB::table('products')->insert([
+                'name' => $x,
+                'sku' => sku($x),
+                'price' => $faker->price(3, 200, false),
+                'tag_id' => rand(1, 13),
+
+            ]);
+        }
     }
 }
-
