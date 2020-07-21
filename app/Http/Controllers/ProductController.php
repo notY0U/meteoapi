abb557cd82fcadd2a70aa43819e7b254f5e53bb8
Dovydas Gražys <57098864+notY0U@users.noreply.github.com>
2020-07-21 15:18:33 +0300

    configure product controller for data handling #18

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -14,82 +14,20 @@ class ProductController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Weather $weather, Request $request)
     {
-    //     
-        $weather = Weather::currentWeather($city)->get('conditionCode');
 
-        // dd($weather);
+        $weather = Weather::currentWeather($request->city)->get('conditionCode');
+        $city = $request->city;
+        $products=Product::where('tag', $weather)->get();
+        return view('product.index', ['weather' => $weather, 'city' => $city, 'products'=> $products]);
 
-        return view('product.index', ['weather' => $weather]);
     }
-
+    
     /**
      * Show the form for creating a new resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function create(Request $request)
-    {
-        $city = $request->city;
-
-        return view('product.create');
-        
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Product  $product
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Product $product)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Product  $product
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Product $product)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Product  $product
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Product $product)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Product  $product
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Product $product)
-    {
-        //
-    }
+    
 }
