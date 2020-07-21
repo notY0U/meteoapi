fe9c7b22c5de540f8bef8375e75544a30aa8ab31
Dovydas Gražys <57098864+notY0U@users.noreply.github.com>
2020-07-21 13:54:23 +0300

    add and configure REST API as service #4

---
+++ app/Services/Weather.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\Http;
+
+class Weather {
+
+    
+    public static function currentWeather(string $argument){
+        
+        /*adjust timezone if necessary
+        * $timezone = 'Europe/Vilnius'; 
+        * $date = new DateTime('now', new DateTimeZone($timezone));
+         */   
+
+        $weather = array_map(function($array){
+            return ['forecastTimeUtc' => $array['forecastTimeUtc'],'conditionCode' => $array['conditionCode']];
+            
+        }, Http::get( 'https://api.meteo.lt/v1/places/'.$argument.'/forecasts/long-term')->json()['forecastTimestamps']);
+        
+        $current_weather = collect($weather[3])->only('conditionCode');
+
+        return $current_weather;
+        
+    }
+
+}
+
+        // dd($current_weather);
