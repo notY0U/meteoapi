528159df963d24780fe09d3811dbd3455e9b5f79
notY0U <57098864+notY0U@users.noreply.github.com>
2020-07-27 15:24:56 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,6 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route :: group ([ 'prefix' => 'products/recommended' ], function (){
+Route :: group ([ 'prefix' => 'products' ], function (){
     Route :: get ( '' , 'ProductController@index' ) -> name ( 'product.index' );
 });
